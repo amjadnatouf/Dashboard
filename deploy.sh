18d8ac49e0e104f13fd77f91aca5538cff5bcd06
amjad <na80@hotmail.com>
2022-05-30 11:12:43 +0200

    change

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:amjadnatouf/dashboard.git master:gh-pages
+git push -f git@github.com:amjadnatouf/dashboard.git main:gh-pages
 
 cd -
